b32442d87ffebddd1885581796faa14392c4a7ba
david <davidjhoyjr@gmail.com>
2022-07-17 14:42:40 -0400

    commit tree 9be4d7d33e51b725e1b9bb036a7a02fa7dae13f4

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,31 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else  
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+
+  end
+
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+ 
+
 end
+
+
